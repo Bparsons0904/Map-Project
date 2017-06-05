d681b7413885ba028a539b6a409e12149a906f51
Bob Parsons <Deadstylebp@gmail.com>
2017-06-04 19:47:03 -0500

    Initial Markers Working, Sidebar Menu Items Added and infobox created.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,70 @@
-var locations = [
+var initialPlaces = [
   {
-    name : 'Swagat',
-    address : '',
+    name : 'USS Wisconsin BB-64',
+    address : '1 Waterside Dr, Norfolk, VA 23510',
+    lat : '36.847681',
+    lng : '-76.295287',
   },
   {
-    name : "Kato's",
-    address : '',
+    name : "The NorVa",
+    address : '317 Monticello Ave, Norfolk, VA 23510',
+    lat : '36.850254',
+    lng : '-76.289670',
+  },
+  {
+    name : "Harbor Park Stadium",
+    address : '150 Park Ave, Norfolk, VA 23510',
+    lat : '36.842657',
+    lng : '-76.278038',
+  },
+  {
+    name : "Virginia Aquarium & Marine Science Center",
+    address : '717 General Booth Blvd, Virginia Beach, VA 23451',
+    lat : '36.821113',
+    lng : '-75.983551',
+  },
+  {
+    name : "Adventure Parasail",
+    address : '300 Winston Salem Ave, Virginia Beach, VA 23451',
+    lat : '36.832082',
+    lng : '-75.974112',
+  },
+  {
+    name : "Tautog's Restaurant",
+    address : '205 23rd St, Virginia Beach, VA 23451',
+    lat : '36.851296',
+    lng : '-75.976501',
+  },
+  {
+    name : "Virginia Zoological Park",
+    address : '3500 Granby St, Norfolk, VA 23504',
+    lat : '36.878735',
+    lng : '-76.279918',
+  },
+  {
+    name : "Mount Trashmore Park",
+    address : '310 Edwin Dr, Virginia Beach, VA 23462',
+    lat : '36.829047',
+    lng : '-76.124740',
+  },
+  {
+    name : "Virginia Beach Fishing Pier",
+    address : '1413 Atlantic Ave, Virginia Beach, VA 23451',
+    lat : '36.843613',
+    lng : '-75.970959',
+  },
+  {
+    name : "Mahi Mah's Seafood",
+    address : '615 Atlantic Avenue, Virginia Beach, VA 23451',
+    lat : '36.835315',
+    lng : '-75.970998',
   },
 ];
 
 
 
-var Map = function() {
-
+var PlaceInformation = function(data) {
+  this.name = ko.observable(data.name);
 
 };
 
@@ -23,20 +75,14 @@ var ViewModel = function() {
 
   this.locationList = ko.observableArray([]);
 
+  initialPlaces.forEach(function(places){
+    self.locationList.push( new PlaceInformation(places) );
+  });
 
-  for (var i = 0; i < locations.length; i++) {
-      var place = locations[i].name;
-      var elem = document.createElement('a');
-      elem.setAttribute('class', 'places_menu')
-      elem.textContent = (place);
-      elem.addEventListener('click', (function(placeCopy) {
-          return function() {
-              console.log(placeCopy);
-              selectMarker(placeCopy);
-          };
-      })(place));
+  this.currentPlace = ko.observable( this.locationList()[0] );
 
-      $('#mySidenav').append(elem);
+  this.setPlace = function(clickedPlace) {
+    self.currentPlace(clickedPlace)
   };
 
 
@@ -49,8 +95,8 @@ ko.applyBindings(new ViewModel());
 // Function to initialize the map within the map div
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {lat: 36.829451, lng: -76.147590},
+    zoom: 12,
+    center: {lat: 36.829047, lng: -76.124740},
     mapTypeControl: true,
     mapTypeControlOptions: {
         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -70,16 +116,16 @@ function initMap() {
     position: google.maps.ControlPosition.RIGHT_BOTTOM
     },
   });
-  var singleLatLng = {lat: 36.829451, lng: -76.147590};
-  // TODO: Create a single marker appearing on initialize -
-  // Create it with the position of the singleLatLng,
-  // on the map, and give it your own title!
-  var marker = new google.maps.Marker({
-    position: singleLatLng,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Hello World!'
-  });
+
+  var marker
+    for (i = 0; i <initialPlaces.length; i++){
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(initialPlaces[i].lat, initialPlaces[i].lng),
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: initialPlaces[i].name,
+      });
+    }
 }
 
 
@@ -103,6 +149,23 @@ function initMap() {
 
 
 /*
+
+for (var i = 0; i < locations.length; i++) {
+    var place = locations[i].name;
+    var elem = document.createElement('a');
+    elem.setAttribute('class', 'places_menu')
+    elem.textContent = (place);
+    elem.addEventListener('click', (function(placeCopy) {
+        return function() {
+            console.log(placeCopy);
+            selectMarker(placeCopy);
+        };
+    })(place));
+
+    $('#mySidenav').append(elem);
+};
+
+
 var availableCats = ['popliner', 'chewie', 'daniel', 'betsy', 'oscar'];
 var catCounts= [0, 0, 0, 0, 0]
 var currentCat = availableCats[0];
