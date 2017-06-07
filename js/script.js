eb052cc1816691939be98e6956540d4bd51c578a
Bob Parsons <Deadstylebp@gmail.com>
2017-06-07 15:17:33 -0500

    Save before redoing infowindow

--- js/script.js
+++ js/script.js
@@ -52,8 +52,8 @@ var initialPlaces = [
     address : '3500 Granby St, Norfolk, VA 23504',
     type : 'Attraction',
     latlng : {
-      lat : 36.878735,
-      lng : -76.279918
+      lat : 36.8768152,
+      lng : -76.2769915
   },
 }, {
     name : "Mount Trashmore Park",
@@ -87,19 +87,13 @@ var PlaceInformation = function(data) {
   this.name = ko.observable(data.name);
   this.latlng = ko.observable(data.latlng);
   this.type = ko.observable(data.type);
-  this.visible = ko.observable( true );
 };
 
 
 var ViewModel = function() {
   var self = this;
 
-
-  self.selectedCategory = ko.observable();
-
   this.newPlace = ko.observable();
-
-
 
   this.locationList = ko.observableArray(initialPlaces);
 
@@ -107,42 +101,87 @@ var ViewModel = function() {
     self.locationList.push( new PlaceInformation(places) );
   });
 
-  this.currentPlace = ko.observable( );
+  this.currentPlace = ko.observable();
 
-  this.markerSelected = function(location) {
-    map.setCenter(location.latlng);
-    map.setZoom(17);
-    self.currentPlace(location);
+  self.photoSuffix = ko.observable();
+  self.photoBase = ko.observable('https://igx.4sqi.net/img/general/width200');
+
+  self.imgSrc = ko.computed(function() {
+    return self.photoBase() + self.photoSuffix();
+  });
+
+  this.infoWindowContent = function(location) {
+/*    newViewModel.imgSrc = ko.observable("<img class='viewImage' src='https://igx.4sqi.net/img/general/width200" + newViewModel.photoSuffix() + "' alt='Image' data-bind=(attr: {src: imgSrc})>"); */
+    var content = "";
+    content += "<h1 class='infoh1'>" + location.name + "</h1>";
+    content += "<h2>" + location.address + "</h2>";
+    content += "<h2>" + location.type + "</h2>";
+    content += "<img class='viewImage' src='images/placehold.png' alt='Image' data-bind=(attr: {src: imgSrc})>";
+    return content
+
   };
 
-};
+
+
+  this.foursquare = function(location) {
+    var oauth = "IQ0J14AEG4RQ2GJHIZMQ54OKSOVV3C5ZQKTHAT44M2WKQ3DB"
+    $.get("https://api.foursquare.com/v2/venues/search?ll=" + location.latlng.lat + "," + location.latlng.lng + "&oauth_token=" + oauth + "&v=20170606",
+     function(data) {
+        var venue = data.response.venues[0];
+        location.id = ko.observable(venue.id);
+    $.get("https://api.foursquare.com/v2/venues/" + location.id() + "/?oauth_token=" + oauth + "&v=20170606",
+      function(details) {
+        var suffix = details.response.venue.bestPhoto.suffix;
+        newViewModel.photoSuffix = suffix;
+      });
+    });
+  };
+
+  this.markerSelected = function(location) {
+    var venue_id;
+    map.setCenter(location.latlng);
+    map.setZoom(17);
+    map.panBy(0,-200);
+    self.currentPlace(location);
+    location.marker.setAnimation(google.maps.Animation.BOUNCE);
+    closeNav()
+    newViewModel.foursquare(location);
+    infowindow.setContent(newViewModel.infoWindowContent(location));
+    infowindow.open(map, location.marker);
+
+
+  };/*markerSelected closing brace */
+
+
+}; /*ViewModel closing brace */
 
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
-    center: {lat: 36.829047, lng: -76.124740},
+    center: {lat : 36.829047, lng : -76.124740},
     mapTypeControl: true,
     mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        position: google.maps.ControlPosition.TOP_RIGHT
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.TOP_RIGHT
     },
     zoomControl: true,
     zoomControlOptions: {
-        position: google.maps.ControlPosition.LEFT_CENTER
+      position: google.maps.ControlPosition.LEFT_CENTER
     },
     scaleControl: true,
     streetViewControl: true,
     streetViewControlOptions: {
-        position: google.maps.ControlPosition.LEFT_BOTTOM
+      position: google.maps.ControlPosition.LEFT_BOTTOM
     },
     fullscreenControl: true,
     fullscreenControlOptions: {
     position: google.maps.ControlPosition.RIGHT_BOTTOM
     },
   });
+    infowindow = new google.maps.InfoWindow();
     markers( newViewModel.locationList() );
-}
+};
 
 
 function markers(locations) {
@@ -150,9 +189,9 @@ function markers(locations) {
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i].latlng),
       map: map,
-      animation: google.maps.Animation.DROP,
       title: String(locations[i].name),
     });
+    newViewModel.locationList()[i].marker = marker;
     google.maps.event.addListener(marker, 'click', (function(location) {
       return function() {
         newViewModel.markerSelected(location);
@@ -162,106 +201,7 @@ function markers(locations) {
 };
 
 var newViewModel = new ViewModel();
-ko.applyBindings( new ViewModel() );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-for (var i = 0; i < locations.length; i++) {
-    var place = locations[i].name;
-    var elem = document.createElement('a');
-    elem.setAttribute('class', 'places_menu')
-    elem.textContent = (place);
-    elem.addEventListener('click', (function(placeCopy) {
-        return function() {
-            console.log(placeCopy);
-            selectMarker(placeCopy);
-        };
-    })(place));
-
-    $('#mySidenav').append(elem);
-};
-
-
-var availableCats = ['popliner', 'chewie', 'daniel', 'betsy', 'oscar'];
-var catCounts= [0, 0, 0, 0, 0]
-var currentCat = availableCats[0];
-
-function findPosition(catName) {
-  var result = availableCats.indexOf(catName);
-  console.log("findPosition" + result);
-  return result
-};
-
-
-function catClick() {
-  var position = findPosition(currentCat)
-  catCounts[position] += 1;
-  console.log("catClick Started");
-  $clickCount.empty();
-  $clickCount.append(catCounts[position]);
-};
-
-
-function capName(name) {
-    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-};
-
-function setCat(newCat) {
-  currentCat = newCat;
-  var position = findPosition(currentCat)
-  $("#image").attr("src", "images/" + currentCat + ".jpg");
-  $name.text(capName(currentCat));
-  $clickCount.empty();
-  $clickCount.append(catCounts[position]);
-};
-
-//$("#image").attr("src", "images/" + currentCat + ".jpg");
-//$name.text(capName(currentCat));
-
-
-//$('#image').click(catClick);
-
-
-/*for (var i = 0; i < availableCats.length; i++) {
-    var cat = availableCats[i];
-    var elem = document.createElement('a');
-    elem.setAttribute('class', 'cat_menu')
-    elem.textContent = capName(cat);
-    elem.addEventListener('click', (function(catCopy) {
-        return function() {
-            console.log(catCopy);
-            setCat(catCopy);
-        };
-    })(cat));
-
-    $('#mySidenav').append(elem);
-};
-*/
-
-
-
-
-
-
-
-
-
-
+ko.applyBindings(new ViewModel());
 
 
 /* Set the width of the side navigation to 250px */
